a8b354575e126c5d6a627df8fc4b5de4ed88f907
nekopath <bm22137@cc.it-hiroshima.ac.jp>
2026-02-05 16:32:50 +0900

    URL対応、不具合修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "static",
   site: "https://nekopath-dev.github.io",
+  base: "/tanaka-yuuna-hp",
   integrations: [tailwind()],
 });
